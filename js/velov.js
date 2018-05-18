5b6a0327807296ecc7fa50a3a65ce688db5bf358
Morgan Taglang <morgan.tag@gmail.com>
2018-05-18 12:50:32 +0200

    fix jquerry position & get api

--- js/velov.js
+++ js/velov.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 $(function(){
     
-    $.ajax({
+    $.get({
         type: 'GET',
-        url: 'https://api.jcdecaux.com/vls/v1/contracts&apiKey={8107a525c841e0b25de543e35fd372943e7b9f1f}',
+        url: 'https://api.jcdecaux.com/vls/v1/contracts?apiKey=8107a525c841e0b25de543e35fd372943e7b9f1f',
+    }, function(data){
+        console.log(data);
     })
 })
-=======
-$(function(){
-    
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.jcdecaux.com/vls/v1/contracts&apiKey={8107a525c841e0b25de543e35fd372943e7b9f1f}',
-    })
-})
->>>>>>> 773823486d27950668f246ca4f9c32e548a83d5b
+
