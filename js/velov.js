d6af8d0bdf05cae3a4a6b015e4ca9a9c83b935b9
Morgan Taglang <morgan.tag@gmail.com>
2018-05-30 17:51:40 +0200

    infos click event marker + fichier css

--- js/velov.js
+++ js/velov.js
@@ -5,16 +5,15 @@ $(function(){
         url: 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=8107a525c841e0b25de543e35fd372943e7b9f1f',
     }, function(data){
         var stations = data;
-        for ( i=0 ; i<=stations.length ; i++){
+        for ( i=0 ; i<stations.length ; i++){
             console.log("Nome de la station : " + stations[i].name + ", latitude : " + stations[i].position.lat + ", longitude : " + stations[i].position.lng)
         }
+        initMap(stations)
     })
 })
 
-// Comment réutiliser stations[i].position dans ligne 36 addMarker addMarker({stations[i].position}) ???
 
-
-function initMap(){
+function initMap(stations){
             var options = {
                 zoom:13,
                 center:{lat:45.750000,lng:4.850000}
@@ -25,15 +24,54 @@ function initMap(){
             
             
             //Add Marker function
-            function addMarker(coords){
+            function addMarker(coords, nomStation){
                 var marker = new google.maps.Marker({
                     position:coords,
+                    name:nomStation,
+                    adresse:adresse,
+                    places:places,
+                    velosDisponibles:velosDisponibles,
                     map:map,
                 });
+        
+            //Evènement au clic sur un marker
+            marker.addListener('click', function() {
+                console.log(marker.name)
+                document.getElementById("nomStation").innerHTML = marker.name ;
+                console.log(marker.adresse)
+                document.getElementById("adresse").innerHTML = marker.adresse ;
+                console.log(marker.places)
+                document.getElementById("nombreDePlaces").innerHTML = marker.places ;
+                console.log(marker.velosDisponibles)
+                document.getElementById("placesDisponibles").innerHTML = marker.velosDisponibles ;
+                });
             }
+    
+            
             //Ajouter des marqueurs
             for (i=0 ; i<stations.length ; i++){
-                addMarker({})
+                var coords = {
+                    lat:stations[i].position.lat,
+                    lng:stations[i].position.lng
+                }
+                var nomStation = stations[i].name ;
+                var adresse = stations[i].address ;
+                var places = stations[i].bike_stands ;
+                var velosDisponibles = stations[i].available_bikes ;
+                
+                console.log(coords)
+                console.log(nomStation)
+                console.log(adresse)
+                addMarker(coords, nomStation)
             };
+            
 }
 
+//Générer les inofs pour le panneau de resrvation de la station
+    function infos(name){
+            var nomStation = document.getElementById("nomStation")
+            console.log(nomStation.textContent);
+            var nomStation = document.getElementById("adresse")
+            var nomStation = document.getElementById("nombreDePlaces")
+            var nomStation = document.getElementById("placesDisponibles")
+}
